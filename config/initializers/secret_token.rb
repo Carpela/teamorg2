68881f0f0d9ca01580f89913131ff31cfa6856fb
Keiran <keiran@carpela.com>
2013-01-10 17:12:37 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamorg2::Application.config.secret_token = 'b0099dd738a9dcd8220a4b0e992aa92915ad8d3969d5810dc5b8e550e41c2f3dbd3df7ccbdfc2e424f58473751913b828df78bffea72b3e36fb79f803d3356b3'
