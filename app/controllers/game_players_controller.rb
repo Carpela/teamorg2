b9eceeef36869401df4c8f7b0e7671429ac84ace
Keiran <keiran@carpela.com>
2013-11-18 13:02:58 +0000

    Added links to select Changed message.

--- app/controllers/game_players_controller.rb
+++ app/controllers/game_players_controller.rb
@@ -1,6 +1,8 @@
 class GamePlayersController < ApplicationController
   def change_status
-    return unless %w(available unavailable).include?(params[:status])
-    GamePlayer.find(params[:game_player_id]).update_attributes status: params[:status]
+    return unless %w(available unavailable selected).include?(params[:status])
+    @gameplayer = GamePlayer.find(params[:game_player_id])
+    @gameplayer.update_attributes status: params[:status]
+    redirect_to game_path(@gameplayer.game), notice: @gameplayer.player.name + ' is ' + params[:status]
   end
 end
