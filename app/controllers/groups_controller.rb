64fc929cc25390164300b144399a48ac85021cbe
Meliborn <denthebat@gmail.com>
2013-11-20 15:26:13 +0300

    Removed current_user present

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,7 @@ class GroupsController < InheritedResources::Base
   end
 
   def add_player
-    @group = current_user.groups.find params[:id]
+    @group = Group.find params[:id]
     @player = Player.new params[:player]
     if @player.save
       @group.players << @player
@@ -17,9 +17,4 @@ class GroupsController < InheritedResources::Base
     end
   end
 
-  protected
-
-  def begin_of_association_chain
-    current_user
-  end
 end
