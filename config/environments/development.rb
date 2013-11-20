095b93f43ce478fef333e0b6bbad3bbb504c2832
Keiran <keiran@carpela.com>
2013-11-20 12:37:27 +0000

    Delete player from game added Dev env variable added DEV_SERVER

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Teamorg2::Application.configure do
   config.action_controller.perform_caching = false
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => ENV['DEV_SERVER'] }
   config.action_mailer.delivery_method = :letter_opener
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = true
