054182194e7f77eba3f62573ae8d8638cb91d524
Keiran <keiran@carpela.com>
2015-02-20 13:46:40 +0000

    Fixed initiailizer problem gocardless.

--- config/initializers/gocardless.rb
+++ config/initializers/gocardless.rb
@@ -1,5 +1,5 @@
 GoCardless.account_details = {
-  :app_id      => 'TEAMORG',
+  :app_id      => ENV['GC_APP_ID'],
   :app_secret  => ENV['GC_APP_SECRET'],
   :token       => ENV['GC_MERCHANT_ACCESS_TOKEN'],
   :merchant_id => ENV['GC_MERCHANT_ID']
